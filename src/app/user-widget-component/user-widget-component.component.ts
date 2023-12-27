39c364f89e9ff0facc877a44235b62c64915a46f
Prashant Deshpande <prashant308.ht@gmail.com>
2023-12-27 18:37:26 +0530

    error-resolved

--- src/app/user-widget-component/user-widget-component.component.ts
+++ src/app/user-widget-component/user-widget-component.component.ts
@@ -12,18 +12,18 @@ export class UserWidgetComponentComponent implements OnInit {
   constructor(private http:SharedService,public router:Router){}
 
   showFiller:boolean = false;
+
   currentUserId:any;
+  currentUserDetails:any;
 
   ngOnInit(){
     this.currentUserId = localStorage.getItem('currentUser')
     this.getCurrentUserData();
   }
 
-  currentUserDetails:any;
   getCurrentUserData(){
     const url = `users/${this.currentUserId}`;
     this.http.getData(url).subscribe((res:any)=>{
-      console.log(res);
       this.currentUserDetails = res;
     })
   }
